97999d7787ad00ca37bd922d8edd3c373d5b632e
Benn <61118590+benedictakim@users.noreply.github.com>
2020-07-01 00:13:55 -0400

    modified:   index.html -added bootstrap divs modified:   script.js -added vars to function weather, ajax for UV new file:   style.css -added header

--- script.js
+++ script.js
@@ -6,24 +6,41 @@ $(document).ready(function(){
         searchweather (searchvalue)
     })
     //onclick for history - local storage of cities, getting from local storage onclick
+    //for (var i = 0; i < lists.length; i++) {
     function searchweather (searchvalue) {
         $.ajax({
             type:"GET",
             url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchvalue + "&appid=33dcb1cd7bc3a96b8d8f723aa3bda2c3&units=imperial",
             dataType: "json",
             success: function (data){
-                console.log(data);
-                //create history links for this search
-                //call our make row function
+            console.log(data);
+           //create history links for this search
+           //call our make row function
                 $("#today").empty()
-                var name = $("<h3>").addClass("card-title").text(data.name)
-                var card = $("<div").addClass("card")
-                var wind = $("<p>").addClass("card-text").text(data.wind.speed)
-                var humidity = $("<p>").addClass("card-text").text(data.main.humidity)
-                var temperature = $("<p>").addClass("card-text").text(data.main.temp)
-                //image
+                    var name = $("<h3>").addClass("card-title").text(data.name) //add current date mm/dd/yyyy
+                    var card = $("<div>").addClass("card")
+                    var temperature = $("<p>").addClass("card-text").text(data.main.temp)
+                    var humidity = $("<p>").addClass("card-text").text(data.main.humidity)
+                    var wind = $("<p>").addClass("card-text").text(data.wind.speed) 
+                    var lat = data.coord.lat
+                    var lon = data.coord.lon                      
+        //     //    var imageURL = "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png"
+        //     //}
+        //    // })
             }
         })
+        // $.ajax({
+        //     type:"GET",
+        //     url: "http://api.openweathermap.org/data/2.5/uvi?appid={33dcb1cd7bc3a96b8d8f723aa3bda2c3}&lat={lat}&lon={lon}",
+        //     dataType: "json",
+        //     success: function (data){
+        //     console.log(data);
+        //         // $("#today").prepend()
+        //         //     var uv = $("<p>").addClass("card-text")()
 
+        //     }
+        // })
     }
 })
+
+//url: 
